29aef3bab80efb50080867c7646f96c578e22e08
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-07-20 11:52:58 -0400

    adding codecov test

--- R/lpcde/tests/testthat/test-lpcde.R
+++ R/lpcde/tests/testthat/test-lpcde.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
+test_that("lpcde pdf est works", {
+  set.seed(42)
+  n=1000
+  x_data = matrix(rnorm(1*n, mean=0, sd=1), ncol=1)
+  y_data = matrix(rnorm(n, mean=0, sd=1))
+  y_grid = stats::quantile(y_data, seq(from=0.1, to=0.9, by=0.1))
+
+# density estimation
+  model1 = lpcde(x_data=x_data, y_data=y_data, y_grid=y_grid, x=0, bw=1.8)
+  summary(model2)
   expect_equal(2 * 2, 4)
 })
